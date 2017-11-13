94c96b1998bed644ab529fa20d92cd44f8609c7b
Bellal Mohamed <mbellal2000@gmail.com>
2017-11-13 02:13:48 +0200

    Added Smart Bubble Sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,6 +6,23 @@ def bubblesort(list):
                 list[i], list[i + 1] = list[i + 1], list[i]
     return list
 
+
+def bubblesortsmart(list):
+    listlength = len(list) - 1
+    swaped = True
+
+    for cycle in range(listlength, 0, -1):
+        if swaped is not True:
+            break
+        swaped = False
+        for i in range(cycle):
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+                swaped = True
+
+    return list
+
 list = [5, 6, 9, 98, 5, 12, 46]
 bubblesort(list)
+bubblesortsmart(list)
 print(list)
